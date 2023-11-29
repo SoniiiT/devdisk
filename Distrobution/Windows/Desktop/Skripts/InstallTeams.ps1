bb9b515bbadfa947f17d8c1209bb61183bc1f4ac
SoniiiT <Toni.Romero@live.de>
2023-11-29 17:33:53 +0100

    modified:   Distrobution/Windows/Desktop/Skripts/InstallTeams.ps1 	new file:   Distrobution/Windows/Server/readme.md

--- Distrobution/Windows/Desktop/Skripts/InstallTeams.ps1
+++ Distrobution/Windows/Desktop/Skripts/InstallTeams.ps1
@@ -1,23 +1,25 @@
 try {
-        $teamsDownloadUrl = "https://go.microsoft.com/fwlink/?linkid=2196060&clcid=0x407&culture=de-de&country=de"
+    $teamsDownloadUrl = "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&download=true"
 
-        $folderPath = "C:\Temp"
-        if (-not (Test-Path $folderPath)) {
-            New-Item -ItemType Directory -Path $folderPath | Out-Null
-        }
-
-        $teamsInstallerPath = "C:\Temp\TeamsInstaller.msix"
-
-        # Download Teams MSIX file
-        Invoke-WebRequest -Uri $teamsDownloadUrl -OutFile $teamsInstallerPath
-        
-        # Install Teams
-        Add-AppxPackage -Path $teamsInstallerPath
-        
-        Write-Host "Teams installed successfully."
-        exit 0
-    } catch {
-        $errorMessage = $_.Exception.Message
-        Write-Host "Teams installation failed. Reason: $errorMessage"
-        exit 1
+    $folderPath = "C:\Temp"
+    if (-not (Test-Path $folderPath)) {
+        New-Item -ItemType Directory -Path $folderPath | Out-Null
     }
+    $teamsInstallerPath = "C:\Temp\msteams_windows_x64.exe"
+
+    # Download Teams installer
+    Invoke-WebRequest -Uri $teamsDownloadUrl -OutFile $teamsInstallerPath
+
+    # Install Teams
+    Start-Process -FilePath $teamsInstallerPath "/s"  -Wait
+
+    # Kill Teams process
+    Get-Process -Name Teams -ErrorAction SilentlyContinue | Stop-Process -Force
+            
+    Write-Host "Teams installed successfully. "
+    exit 0
+} catch {
+    $errorMessage = $_.Exception.Message
+    Write-Host "Teams installation failed. Reason: $errorMessage"
+    exit 1
+}
