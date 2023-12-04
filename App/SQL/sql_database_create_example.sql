4353699e4d66583dae443900826988a02f23b94a
SoniiiT <Toni.Romero@live.de>
2023-12-04 17:25:14 +0100

    modified:   .vscode/settings.json 	modified:   App/SQL/sql_database_create_example.sql

--- App/SQL/sql_database_create_example.sql
+++ App/SQL/sql_database_create_example.sql
@@ -1,17 +1,40 @@
 CREATE DATABASE IF NOT EXISTS example;
 USE example;
 
-CREATE TABLE IF NOT EXISTS Abteilung (
-    AbteilungID INT NOT NULL AUTO_INCREMENT,
-    AbteilungName VARCHAR(255) NOT NULL,
-    PRIMARY KEY (AbteilungID)
+CREATE TABLE IF NOT EXISTS User (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS Mitarbeiter (
-    MitarbeiterID INT NOT NULL AUTO_INCREMENT,
-    Vorname VARCHAR(255) NOT NULL,
-    Nachname VARCHAR(255) NOT NULL,
-    PRIMARY KEY (MitarbeiterID),
-    AbteilungID INT,
-    FOREIGN KEY (AbteilungID) REFERENCES Abteilung(AbteilungID)
+CREATE TABLE IF NOT EXISTS Addresses (
+    id INT NOT NULL AUTO_INCREMENT,
+    plz VARCHAR(128) NOT NULL,
+    city VARCHAR(128) NOT NULL,
+    street VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id),
+    user_id INT, FOREIGN KEY (fk_user_id) REFERENCES User(id)
+);
+
+CREATE TABLE IF NOT EXISTS Products (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    current_price FLOAT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_id INT, FOREIGN KEY (fk_product_id) REFERENCES Products(id),
+    user_id INT, FOREIGN KEY (fk_user_id) REFERENCES User(id),
+    addresses_id INT, FOREIGN KEY (fk_address_id) REFERENCES Addresses(id),
+    order_amount INT NOT NULL,
+    order_price FLOAT NOT NULL,
+    order_date datetime NOT NULL,
+    shipping_date datetime NOT NULL,
+    PRIMARY KEY (id),
 );
