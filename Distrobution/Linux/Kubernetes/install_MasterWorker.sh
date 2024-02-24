b5bcc3528c0d307e169771e7e50eeb00e138e8de
SoniiiT <Toni.Romero@live.de>
2024-02-24 21:06:04 +0100

    Update install_MasterWorker.sh

--- Distrobution/Linux/Kubernetes/install_MasterWorker.sh
+++ Distrobution/Linux/Kubernetes/install_MasterWorker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Ask for the IP of K8s-VI
+echo "Enter the IP of the K8s-VI (Virtual IP):"
+read ip_virtual
+
+# Update and Upgrade the System and adding Firewall Rules
 sudo apt-get update && sudo apt-get upgrade -y
 sudo ufw allow 6443/tcp
 sudo ufw allow 6443/udp
@@ -31,3 +36,11 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt update
 sudo apt install kubeadm kubelet kubectl kubernetes-cni -y
+
+# Fixing Kubernetes
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+# Adding K8s-VI in the /etc/hosts file
+echo "
+$ip_virtual k8s-vi" >> /etc/hosts
